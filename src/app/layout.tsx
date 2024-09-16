b88c3ce551b3863075a91d2a4f4eba34d13672a2
Adnan Husain <adnan@mithyalabs.com>
2024-09-16 17:51:48 +0530

    og info added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,25 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        <title>BQ Tours and Travels</title>
+        <meta
+          name="description"
+          content="Your trusted partner for domestic and international travel planning."
+        />
+        <meta
+          name="keywords"
+          content="Tours, travels, visa, passport, nagpur, "
+        />
+        <meta name="author" content="Mustafa Qubbawala" />
+        <meta property="og:title" content="BQ Tours and Travels" />
+        <meta
+          property="og:description"
+          content="Your trusted partner for domestic and international travel planning."
+        />
+        <meta property="og:url" content="https://bqtoursandtravels.com" />
+        <meta property="og:image" content="/assets/ogImage.png" />
+        <meta property="og:type" content="website" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script
           defer
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}`}
